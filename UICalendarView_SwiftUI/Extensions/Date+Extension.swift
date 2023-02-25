a7ccbdfab8c7f7f192586289b60ac41d06affca0
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-02-25 03:39:21 +0100

    First commit - uploaded starter project

---
+++ UICalendarView_SwiftUI/Extensions/Date+Extension.swift
@@ -0,0 +1,16 @@
+//
+// Created for UICalendarView_SwiftUI
+// by Stewart Lynch on 2022-06-28
+// Using Swift 5.0
+//
+// Follow me on Twitter: @StewartLynch
+// Subscribe on YouTube: https://youTube.com/StewartLynch
+//
+
+import Foundation
+
+extension Date {
+    func diff(numDays: Int) -> Date {
+        Calendar.current.date(byAdding: .day, value: numDays, to: self)!
+    }
+}
---
+++ UICalendarView_SwiftUI/Views/CalendarViewTab/EventsCalendarView.swift
@@ -0,0 +1,25 @@
+//
+// Created for UICalendarView_SwiftUI
+// by Stewart Lynch on 2022-06-28
+// Using Swift 5.0
+//
+// Follow me on Twitter: @StewartLynch
+// Subscribe on YouTube: https://youTube.com/StewartLynch
+//
+
+import SwiftUI
+
+struct EventsCalendarView: View {
+    var body: some View {
+        NavigationStack {
+            Text("We will code this")
+                .navigationTitle("Calendar View")
+        }
+    }
+}
+
+struct EventsCalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        EventsCalendarView()
+    }
+}
