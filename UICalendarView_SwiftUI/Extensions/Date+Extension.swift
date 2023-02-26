fbf70d1f2408b93ff7352c0f4100cc3a2c5ae881
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-02-26 03:51:40 +0100

    Completed UICalendarView in SwiftUI

--- UICalendarView_SwiftUI/Extensions/Date+Extension.swift
+++ UICalendarView_SwiftUI/Extensions/Date+Extension.swift
@@ -13,4 +13,8 @@ extension Date {
     func diff(numDays: Int) -> Date {
         Calendar.current.date(byAdding: .day, value: numDays, to: self)!
     }
+    
+    var startOfDay: Date {
+        Calendar.current.startOfDay(for: self)
+    }
 }
---
+++ UICalendarView_SwiftUI/Views/CalendarViewTab/DaysEventsListView.swift
@@ -0,0 +1,58 @@
+//
+//  DaysEventsListView.swift
+//  UICalendarView_SwiftUI
+//
+//  Created by Marcin Jędrzejak on 26/02/2023.
+//
+
+import SwiftUI
+
+struct DaysEventsListView: View {
+    @EnvironmentObject var eventStore: EventStore
+    @Binding var dateSelected: DateComponents?
+    @State private var formType: EventFormType?
+    var body: some View {
+        NavigationStack {
+            Group {
+                if let dateSelected {
+                    let foundEvents = eventStore.events
+                        .filter { $0.date.startOfDay == dateSelected.date!.startOfDay }
+                    List {
+                        ForEach(foundEvents) { event in
+                            ListViewRow(event: event, formType: $formType)
+                                .swipeActions {
+                                    Button(role: .destructive) {
+                                        eventStore.delete(event)
+                                    } label: {
+                                        Image(systemName: "trash")
+                                    }
+
+                                }
+                                .sheet(item: $formType) { $0 }
+                        }
+                    }
+                }
+            }
+            .navigationTitle(dateSelected?.date?.formatted(date: .long, time: .omitted) ?? "")
+        }
+    }
+}
+
+struct DaysEventsListView_Previews: PreviewProvider {
+    static var dateComponents: DateComponents {
+        var dateComponents = Calendar.current.dateComponents(
+            [.year,
+             .month,
+             .day,
+             .hour,
+             .minute],
+            from: Date())
+        dateComponents.timeZone = TimeZone.current
+        dateComponents.calendar = Calendar(identifier: .gregorian)
+        return dateComponents
+    }
+    static var previews: some View {
+        DaysEventsListView(dateSelected: .constant(dateComponents))
+            .environmentObject(EventStore(preview: true))
+    }
+}
